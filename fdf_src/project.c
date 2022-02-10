e64b60e36febe7f44d5152c5b4a90221b0ab4b1b
Robin Burri <robinburri@tutanota.com>
2022-02-10 09:28:27 +0100

    better projection

--- fdf_src/project.c
+++ fdf_src/project.c
@@ -6,7 +6,7 @@
 /*   By: rburri <rburri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/12 09:07:35 by rburri            #+#    #+#             */
-/*   Updated: 2022/02/07 11:28:42 by rburri           ###   ########.fr       */
+/*   Updated: 2022/02/10 09:23:04 by rburri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,17 +33,21 @@ t_point	project(t_point p, t_map *map)
 	else
 		tmp = (HEIGHT / map->height / 2);
 	if (map->z_range > 0 && (HEIGHT / map->z_range / 2) < tmp)
-		zoom = (HEIGHT / map->z_range / 2);
+		zoom = (HEIGHT / map->z_range / 1.1);
 	else
 		zoom = tmp;
 	p.x *= zoom;
 	p.y *= zoom;
 	p.z *= zoom;
+	if (map->z_range >= 20)
+		p.z /= 2;
+	if (map->z_range / map->width > 3)
+	{
+		p.x *= 2;
+		p.y *= 2;
+	}
 	iso(&p.x, &p.y, p.z);
 	p.x += (WIDTH / 2) - (zoom * map->width / 2) + (map->height * zoom / 2);
-	if (map->z_range > 0 && (HEIGHT / map->z_range / 2) > zoom)
-		p.y += (HEIGHT / 2) - (zoom * map->height / 2);
-	else
-		p.y += (HEIGHT / 2) - (zoom * map->z_range / 20);
+	p.y += (HEIGHT / 2) - (zoom * map->height / 4);
 	return (p);
 }
